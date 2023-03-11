e84f4503131eebc12a579e2b7cb9dd2b7df3e7f8
Nicholas Eisenberg <nickeisenberg@MacBook-Pro-7.local>
2023-03-10 17:04:52 -0800

    pushing changes

--- init.vim
+++ init.vim
@@ -10,6 +10,7 @@ set encoding=utf-8
 set backspace=indent,eol,start
 set cursorline
 set cursorcolumn
+" set relativenumber
 set number
 nnoremap <leader>h :set hlsearch! hlsearch?<CR> 
 nnoremap <leader>n :set nu! nu? <CR>
@@ -45,6 +46,15 @@ Plug 'saadparwaiz1/cmp_luasnip'
 Plug 'rafamadriz/friendly-snippets'
 Plug 'neovim/nvim-lspconfig'
 
+"-----jupyter plugin-----
+Plug 'luk400/vim-jukit' 
+
+"-----sql-----
+Plug 'nanotee/sqls.nvim'
+
+"-----indentation guide-----
+Plug 'lukas-reineke/indent-blankline.nvim'
+
 " Plug 'williamboman/nvim-lsp-installer'
 " Plug 'hrsh7th/cmp-copilot'
 call plug#end()
@@ -57,10 +67,11 @@ let g:slime_default_config = {"socket_name": get(split($TMUX, ","), 0), "target_
 " plugin configs
 lua require('plug_config.nerd-tree')
 lua require('plug_config.treesitter')
-" lua require('plug_config.tokyonight')
-lua require('plug_config.gruvbox')
+lua require('plug_config.tokyonight')
+" lua require('plug_config.gruvbox')
 " lua require('plug_config.coc')
 lua require('plug_config.nvim-cmp')
 lua require('plug_config.lsp_config')
 " lua require('plug_config.lsp_installer')
 lua require('plug_config.nvimR')
+lua require('plug_config.indent_blankline')
--- lua/plug_config/lsp_config.lua
+++ lua/plug_config/lsp_config.lua
@@ -1,28 +1,29 @@
-require('mason').setup()
+require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua"}
+	--  ensure_installed = { "sumneko_lua"}
+	ensure_installed = { "lua_ls" },
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").sumneko_lua.setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.stdpath "config" .. "/lua"] = true,
-        },
-      },
-    },
-  }
-}
+require("lspconfig").lua_ls.setup({
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+})
 
 -- Python
-require'lspconfig'.pyright.setup{}
+require("lspconfig").pyright.setup({})
 -- R
-require'lspconfig'.r_language_server.setup{}
+require("lspconfig").r_language_server.setup({})
