02a94f97e449f085bf927acf6b7f22e2fd085131
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-12 11:01:25 -0700

    changed some slime settings

--- lua/plugins/slime.lua
+++ lua/plugins/slime.lua
@@ -8,8 +8,9 @@ return {
         let g:slime_preserve_curpos = 0
         let g:slime_no_mappings = 1
         xmap <c-c><c-c> <Plug>SlimeRegionSend <Esc> `>
-        nmap <c-c><c-c> <Plug>SlimeParagraphSend
-        nmap <c-c>v     <Plug>SlimeConfig
+        nmap <c-c><c-i> <Plug>SlimeParagraphSend
+        nmap <c-c><c-c> <Plug>SlimeParagraphSend <Esc> }
+        nmap <c-c><v    <Plug>SlimeConfig
       ]]
     )
   end,
