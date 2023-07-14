a138a3946e10a3cf13fd5d43b1e334861b59d69e
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-13 17:42:25 -0700

    Ex remap name

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ve", vim.cmd.Ex)
 vim.keymap.set("n", "<S-h>", vim.cmd.BufferLineCyclePrev)
 vim.keymap.set("n", "<S-l>", vim.cmd.BufferLineCycleNext)
 vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
