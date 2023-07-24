029fe355bf508c1efd242d1e62e5e67a9a48cad7
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-24 16:59:14 -0700

    new color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,21 @@
 return {
-    {
+  {
     "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       vim.cmd([[colorscheme tokyonight-night]])
     end,
+    enabled = false
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd([[colorscheme kanagawa-wave]])
+    end,
+    enabled = true
   },
 }
 
