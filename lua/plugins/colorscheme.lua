eddfb2847ecab38d75b923365dcd4f627446584a
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-25 08:59:21 -0700

    changed the color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -15,6 +15,15 @@ return {
     config = function()
       vim.cmd([[colorscheme kanagawa-wave]])
     end,
+    enabled = false
+  },
+  {
+    'rose-pine/neovim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd([[colorscheme rose-pine]])
+    end,
     enabled = true
   },
 }
---
+++ testing/colorscheme.py
@@ -0,0 +1,29 @@
+'''
+Text to see how the color scheme looks
+'''
+
+
+import numpy as np
+
+np.linspace(
+    0, 1, 100
+)
+
+class foo:
+
+    def __init__(self):
+        self.x = 2
+    
+    # text text
+    @property
+    def bar(self):
+        '''
+            text text
+        '''
+        try:
+            _ = self.x
+        except:
+            _ = False
+        return None
+
+
